759bd56265aba6d6c42c6eeae878996c12bcacae
1651680599 <36470299+1651680599@users.noreply.github.com>
2022-05-21 15:43:55 +0800

    Update Today.java
    import java.util.Date;

--- src/main/java/com/imooc/Today.java
+++ src/main/java/com/imooc/Today.java
@@ -1,8 +1,12 @@
 package com.imooc;
 
+import java.util.Date;
+
 public class Today {
   public static void main(String[] args) {
         System.out.println("today time is :"+System.currentTimeMillis());
         System.out.println("now time is :"+System.currentTimeMillis());
+            System.out.println("cuurent time is :"+System.currentTimeMillis()+new Date());
+
     }
 }
