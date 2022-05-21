29103d5f947f0be85c06052832916adb1be7cdff
1651680599 <1651680599@qq.com>
2022-05-21 15:42:08 +0800

    add now time

--- src/main/java/com/imooc/Today.java
+++ src/main/java/com/imooc/Today.java
@@ -1,8 +1,9 @@
 package com.imooc;
 
 public class Today {
-  public static void main(String[] args) {
-        System.out.println("today time is :"+System.currentTimeMillis());
-        System.out.println("now time is :"+System.currentTimeMillis());
+    public static void main(String[] args) {
+        System.out.println("today time is :" + System.currentTimeMillis());
+        System.out.println("now time is :" + System.currentTimeMillis());
+        System.out.println("now time is :" + System.currentTimeMillis());
     }
 }
