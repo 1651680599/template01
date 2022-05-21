65ffae160be27c0ac36663d5306ef1a884569d93
1651680599 <1651680599@qq.com>
2022-05-21 15:45:57 +0800

    add 3

--- src/main/java/com/imooc/Today.java
+++ src/main/java/com/imooc/Today.java
@@ -1,9 +1,11 @@
 package com.imooc;
 
+import java.util.Date;
+
 public class Today {
     public static void main(String[] args) {
         System.out.println("today time is :" + System.currentTimeMillis());
         System.out.println("now time is :" + System.currentTimeMillis());
-        System.out.println("now time is :" + System.currentTimeMillis());
+        System.out.println("now time is :" + System.currentTimeMillis()+new Date());
     }
 }
